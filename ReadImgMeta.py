40214b28d1ce45dbe33cde3dc1f594d7911b99f9
Luai C9 <luai.hassun@gmail.com>
2022-01-26 14:26:51 +0100

    Initial commit

---
+++ ReadImgMeta.py
@@ -0,0 +1,71 @@
+import PIL
+from PIL import Image
+from PIL.ExifTags import TAGS
+from PIL.ExifTags import GPSTAGS
+
+
+def get_exif(imgpath):
+    image = Image.open(imgpath)
+    image.verify()
+    return image._getexif()
+
+
+def get_labeled_exif(exif):
+    labeled = {}
+    for (key, val) in exif.items():
+        labeled[TAGS.get(key)] = val
+
+    return labeled
+
+
+def get_geotagging(exif):
+    if not exif:
+        raise ValueError("No EXIF metadata found")
+
+    geotagging = {}
+    for (idx, tag) in TAGS.items():
+        if tag == 'GPSInfo':
+            if idx not in exif:
+                raise ValueError("No EXIF geotagging found")
+
+            for (key, val) in GPSTAGS.items():
+                if key in exif[idx]:
+                    geotagging[val] = exif[idx][key]
+
+    return geotagging
+
+
+def imageoutput(imgpath):
+    exif = get_exif(imgpath)
+    geotags = get_geotagging(exif)
+    imgoutput = str(geotags)
+    return imgoutput
+
+
+def get_decimal_from_dms(dms, ref):
+    degrees = dms[0][0] / dms[0][1]
+    minutes = dms[1][0] / dms[1][1] / 60.0
+    seconds = dms[2][0] / dms[2][1] / 3600.0
+
+    if ref in ['S', 'W']:
+        degrees = -degrees
+        minutes = -minutes
+        seconds = -seconds
+
+    return round(degrees + minutes + seconds, 5)
+
+
+def get_coordinates(geotags):
+    lat = get_decimal_from_dms(geotags['GPSLatitude'], geotags['GPSLatitudeRef'])
+
+    lon = get_decimal_from_dms(geotags['GPSLongitude'], geotags['GPSLongitudeRef'])
+
+    return lat, lon
+
+
+# TODO Umwandlung von Koordinaten
+
+
+# print(get_coordinates(geotags))
+
+print(get_exif('picture.jpg'))
